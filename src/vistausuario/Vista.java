9e48edfcf17813683ae865fd6279e3300f90697c
amalaucin@espol.edu.ec <amalucin@espol.edu.ec>
2025-06-20 16:16:27 -0500

    Creacion paquete con clase que almacena la vista usuario

---
+++ src/vistausuario/Vista.java
@@ -0,0 +1,23 @@
+package vistausuario;
+
+import java.util.Scanner;
+
+public class Vista {
+
+    public void mostrarMenu(){
+        System.out.println("MENU PRINCIPAL");
+        System.out.println("1. Administrar Clientes");
+        System.out.println("2. Administrar Proveedores");
+        System.out.println("3. Administrar Tecnicos");
+        System.out.println("4. Administrar Servicios");
+        System.out.println("5. Generar orden de servicios");
+        System.out.println("6. Registrar falta de insumos");
+        System.out.println("7. Generar facturas a empresas");
+        System.out.println("8. Reporte de ingresos por servicio");
+        System.out.println("9. Reporte de atenciones por tecnico");
+        System.out.println("10. Salir");
+
+    }
+
+    
+}
